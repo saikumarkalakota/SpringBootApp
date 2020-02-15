fdad99fbb2209317d303bebc482e20d818509eb2
saikumarkalakota <58147758+saikumarkalakota@users.noreply.github.com>
2020-02-16 01:25:22 +0530

    Refer Comments for more understanding of implementation

--- course-api/src/main/java/brains/springbootapp/topic/TopicService.java
+++ course-api/src/main/java/brains/springbootapp/topic/TopicService.java
@@ -15,7 +15,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	
+	/**
+	 * new ArrayList ( ) creates a mutable arrayList each time this service is called.
+	 * This enables ArrayList to be updated. In general Array.asList is treated as String
+	 * which is immutable.
+	 */
 	private List<Topic> topics = new ArrayList<> (Arrays.asList(
 			new Topic ("spring","Spring framework", "Spring framework Description"),
 			new Topic ("java","java framework", "java framework Description"),
@@ -30,6 +34,13 @@ public class TopicService {
 
 
 	public Topic getTopic(String id) {
+		/**
+		 * topics is a List which is converted to
+		 * stream
+		 * Then filter is applied to get the id that matches first with
+		 * passed argumet 'id' & fetched via get().
+		 * -> is lambda expression ( efficient way of searching).
+		 */
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 	}
 
