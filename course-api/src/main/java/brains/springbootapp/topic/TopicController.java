92937860e360acb1c8fb66150c26033213b6ccab
saikumarkalakota <58147758+saikumarkalakota@users.noreply.github.com>
2020-02-16 00:36:14 +0530

    Controller & Service separated out as 2 diff classes

--- course-api/src/main/java/brains/springbootapp/topic/TopicController.java
+++ course-api/src/main/java/brains/springbootapp/topic/TopicController.java
@@ -3,20 +3,24 @@ package brains.springbootapp.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	/**
+	 * We are below private member variable to instruct Spring to use the service instance 
+	 * created  at start-up.
+	 * @Autowired marks that below variable needs dependency (@Service) injection
+	 */
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		
-		return Arrays.asList(
-				new Topic ("spring","Spring framework", "Spring framework Description"),
-				new Topic ("java","java framework", "java framework Description"),
-				new Topic ("DotNet","DotNet framework", "DotNet framework Description")
-				);
+		return topicService.getAllTopics();
 	}
 
 }
